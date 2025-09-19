a794382b2212558ee9b330f17bc15e00dfca0e22
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2025-09-19 16:06:08 +0700

    model and migrate tasks

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    //
+
+    protected $fillable = [
+        'user_id',
+        'card_id',
+        'parent_id',
+        'title',
+        'is_completed',
+    ];
+}
