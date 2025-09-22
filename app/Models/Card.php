828974b5a479b570d6f89127ed69c237de8f77dd
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2025-09-22 09:23:11 +0700

    relations

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use App\Enums\CardPriority;
 use App\Enums\CardStatus;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Card extends Model
 {
@@ -28,5 +31,30 @@ class Card extends Model
             'status' => CardStatus::class,
             'priority' => CardPriority::class
         ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function workspace(): BelongsTo
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    public function attachments(): HasMany
+    {
+        return $this->hasMany(Attachment::class);
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function members(): MorphMany
+    {
+        return $this->morphMany(Member::class, 'memberable');
     }
 }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -3,10 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Member extends Model
 {
     //
 
     protected $guarded = [];
+
+    protected $with = ['user'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function memberable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
