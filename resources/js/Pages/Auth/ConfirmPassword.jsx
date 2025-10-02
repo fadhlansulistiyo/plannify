9c26e9ba58eda2f9383c5549dce7eae4901e2782
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2025-10-02 15:51:27 +0700

    refactoring page confirm password

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -1,54 +1,72 @@
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
+import { Button } from '@/Components/ui/button';
+import { Card, CardContent, CardHeader } from '@/Components/ui/card';
 import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, useForm } from '@inertiajs/react';
+import { Link, useForm } from '@inertiajs/react';
+import { useEffect } from 'react';
 
 export default function ConfirmPassword() {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        password: '',
-    });
+  const { data, setData, post, processing, errors, reset } = useForm({
+    password: '',
+  });
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route('password.confirm'), {
-            onFinish: () => reset('password'),
-        });
+  useEffect(() => {
+    return () => {
+      reset('password');
     };
+  }, []);
 
-    return (
-        <GuestLayout>
-            <Head title="Confirm Password" />
+  const submit = (e) => {
+    e.preventDefault();
 
-            <div className="mb-4 text-sm text-gray-600">
-                This is a secure area of the application. Please confirm your password before continuing.
+    post(route('password.confirm'));
+  };
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Card>
+          <CardHeader>
+            <Link href="/" className="text-4xl font-black leading-relaxed tracking-tighter">
+              Plannify<span className="text-red-500">.</span>
+            </Link>
+            <h2 className="text-left text-lg font-medium leading-9 tracking-tight text-muted-foreground">
+              This is a secure area of the application. Please confirm your password before continuing.
+            </h2>
+          </CardHeader>
+          <CardContent>
+            <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" onSubmit={submit}>
+                {/* form */}
+                <div>
+                  <InputLabel htmlFor="email" value={'Email'} />
+                  <TextInput
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={data.password}
+                    className="mt-1 block w-full"
+                    isFocused={true}
+                    placeholders="password Address"
+                    onChange={(e) => setData('password', e.target.password)}
+                    onErrors={errors.password && <InputError className="mt-1" message={errors.password} />}
+                  />
+                </div>
+
+                <div>
+                  <Button type="submit" variant="red" className="w-full" disabled={processing}>
+                    Confirm
+                  </Button>
+                </div>
+              </form>
             </div>
-
-            <form onSubmit={submit}>
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        isFocused={true}
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4 flex items-center justify-end">
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Confirm
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
-    );
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
+
+ConfirmPassword.layout = (page) => <GuestLayout children={page} title="Confirm Password" />;
